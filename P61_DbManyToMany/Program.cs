c166c50b198c370d5e63a6e0962a7c342aa454c2
Marius Meskauskas (uif06769) <marius.meskauskas@continental-corporation.com>
2023-11-23 05:36:03 +0200

    njum

--- P61_DbManyToMany/Program.cs
+++ P61_DbManyToMany/Program.cs
@@ -39,21 +39,49 @@ namespace P61_DbManyToMany
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Many to Many!");
+			Console.WriteLine("Hello, Many to Many!");
+			var dbContext = new BookContext(new DbContextOptionsBuilder<BookContext>()
+				.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=NETUA2_books;Trusted_Connection=True;")
+				.Options);
+			dbContext.Database.EnsureDeleted();
+			dbContext.Database.EnsureCreated();
 
-            //var dbContext = new BookContext(new DbContextOptionsBuilder<BookContext>()
-            //    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=NETUA2_books;Trusted_Connection=True;")
-            //    .Options);
-            //dbContext.Database.EnsureDeleted();
-            //dbContext.Database.EnsureCreated();
-            ////dbContext.SaveChanges();
-
-            var rowling = new Author { LastName = "Rowling" };
+			var rowling = new Author { LastName = "Rowling" };
             var hawkins = new Author { LastName = "Hawkins" };
             var carnagie = new Author { LastName = "Carnagie" };
             var adams = new Author { LastName = "Adams" };
 
+			dbContext.Authors.AddRange(rowling, hawkins, carnagie, adams);
+			dbContext.SaveChanges();
 
-        }
+			var hobbit = new Book { Title = "The Hobbit", Year = 1937, Author = rowling };
+			var lordOfTheRings = new Book { Title = "The Lord of the Rings", Year = 1954, Author = rowling };
+			var silmarillion = new Book { Title = "The Silmarillion", Year = 1977, Author = rowling }; //Silmariljonas, Dþonas Ronaldas Reuelis Tolkinas
+			var hitchhikersGuide = new Book { Title = "The Hitchhiker's Guide to the Galaxy", Year = 1979, Author = adams };
+			var dune = new Book { Title = "Dune", Year = 1965, Author = adams };
+			var endersGame = new Book { Title = "Ender's Game", Year = 1985, Author = carnagie };
+
+			dbContext.Books.AddRange(hobbit, lordOfTheRings, silmarillion, hitchhikersGuide, dune, endersGame);
+			dbContext.SaveChanges();
+
+			var catAdventure = new Category { CategoryName = "Adventure" };
+			var catScience = new Category { CategoryName = "Science Fiction" };
+
+			dbContext.Categories.AddRange(catAdventure, catScience);
+			dbContext.SaveChanges();
+
+			dbContext.BookCategories.Add(new BookCategory { Category = catAdventure, Book = hobbit });
+			dbContext.BookCategories.Add(new BookCategory { Category = catAdventure, Book = lordOfTheRings });
+			dbContext.BookCategories.Add(new BookCategory { Category = catAdventure, Book = silmarillion });
+
+			dbContext.BookCategories.Add(new BookCategory { Category = catScience, Book = hitchhikersGuide });
+			dbContext.BookCategories.Add(new BookCategory { Category = catScience, Book = dune });
+			dbContext.BookCategories.Add(new BookCategory { Category = catScience, Book = endersGame });
+			dbContext.BookCategories.Add(new BookCategory { Category = catScience, Book = hobbit });
+
+			dbContext.SaveChanges();
+
+
+		}
     }
 }
